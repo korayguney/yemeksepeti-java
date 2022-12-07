3b1158f5e8bc211c6b2b9942717947abc55a9ca7
Koray Guney <koray.guney@hotmail.com>
2022-12-07 15:56:50 +0300

    fourth day

--- yemeksepeti-03/src/com/yemeksepeti/bmicalculator/BmiCalculator.java
+++ yemeksepeti-03/src/com/yemeksepeti/bmicalculator/BmiCalculator.java
@@ -1,4 +1,30 @@
 package com.yemeksepeti.bmicalculator;
 
+import java.util.Scanner;
+
 public class BmiCalculator {
+    public static void main(String[] args) {
+        double height;
+        double weight;
+        double bmi;
+
+        System.out.println("Enter your weight(enter in kg)");
+        Scanner scanner = new Scanner(System.in);
+        weight = scanner.nextDouble();
+        System.out.println("Enter your height(enter in m)");
+        height = scanner.nextDouble();
+       // bmi = weight / (height * height);
+        bmi = weight / Math.pow(height, 2);
+
+        if (bmi < 18.5) {
+            System.out.println("You are underweight");
+        } else if (bmi > 18.5 && bmi < 24.9) {
+            System.out.println("You are normal");
+        } else if (bmi > 25.0 && bmi < 29.9) {
+            System.out.println("You are overweight");
+        } else if (bmi > 30.0) {
+            System.out.println("You are very overweight");
+        }
+
+    }
 }
