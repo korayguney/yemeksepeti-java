4ed7228ecddd6078ea43f901460faf6bf904a4f5
haci56 <haci.altay@yemeksepeti.com>
2022-12-07 16:54:24 +0300

    refactor 3

--- yemeksepeti-03/src/com/yemeksepeti/bmicalculator/BmiCalculator.java
+++ yemeksepeti-03/src/com/yemeksepeti/bmicalculator/BmiCalculator.java
@@ -7,7 +7,7 @@ public class BmiCalculator {
 
     private static Double weight;
     private static Double height;
-    static Double result;
+    static Double bmi;
     private static Scanner myObj = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -18,13 +18,13 @@ public class BmiCalculator {
     }
 
     public static void whatIsMyCondition(){
-        if(result < 18.5){
+        if(bmi < 18.5){
             System.out.println(" You are underweigh");
-        }else if(result > 18.5 && result<24.9){
+        }else if(bmi > 18.5 && bmi<24.9){
             System.out.println(" You are normal");
-        }else if(result > 25.0 && result<29.9){
+        }else if(bmi > 25.0 && bmi<29.9){
             System.out.println(" You are overweigh");
-        }else if(result > 30.0 ){
+        }else if(bmi > 30.0 ){
             System.out.println(" You are very overweigh");
         }else{
             System.out.println(" You entered incorrect values");
@@ -32,20 +32,18 @@ public class BmiCalculator {
     }
 
     public static Double getBmi (){
-        result = weight/(height*height);
-        System.out.println("Result is :"+ result);
-        return result;
+        bmi = weight/(height*height);
+        System.out.println("Bmi is :"+ bmi);
+        return bmi;
     }
 
     public static Double getWeigh(){
         System.out.println("Please enter weight");
-        weight = myObj.nextDouble();
-        return weight;
+        return weight = myObj.nextDouble();
     }
 
     public static Double getHeigh(){
         System.out.println("Please enter height");
-        height = myObj.nextDouble();
-        return height;
+        return height = myObj.nextDouble();
     }
 }
