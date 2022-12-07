914d7b85ee261bb78ba6a1fa2cd0c49ca0249d8e
Merve Parlak <parlakmervepmm@gmail.com>
2022-12-07 15:46:01 +0300

    merve's code

--- yemeksepeti-03/src/com/yemeksepeti/bmicalculator/BmiCalculator.java
+++ yemeksepeti-03/src/com/yemeksepeti/bmicalculator/BmiCalculator.java
@@ -1,4 +1,32 @@
 package com.yemeksepeti.bmicalculator;
 
+import java.util.Scanner;
+
 public class BmiCalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter height");
+        Scanner input = new Scanner(System.in);
+        double height = input.nextDouble();
+        System.out.println("Height" + height);
+
+        System.out.println("Enter weight");
+        Scanner input1 = new Scanner(System.in);
+        double weight = input.nextDouble();
+        System.out.println("Weight" + weight);
+
+       double bmi= weight / (height * height);
+
+       if(bmi < 18.5){
+           System.out.println("underweight");
+       }
+       else if(18.5 < bmi && bmi < 24.9){
+           System.out.println("normal");
+       }
+       else if( 25.0 < bmi && bmi < 29.9){
+           System.out.println("overweight");
+       }
+       else if( 30.0 <= bmi){
+           System.out.println("very overweight");
+       }
+    }
 }
